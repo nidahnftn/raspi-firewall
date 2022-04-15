cc620866b3942202b2373d59c4573bff73f9794e
nidahnftn <hanifatunnida99@gmail.com>
2022-04-15 13:49:44 +0700

    edited script

--- start.sh
+++ start.sh
@@ -41,7 +41,7 @@ wpa_key_mgmt=WPA-PSK
 wpa_pairwise=TKIP
 rsn_pairwise=CCMP
 ssid=raspi
-wpa_passphrase=raspi12345
+wpa_passphrase=thisispassword123
 ' | sudo tee -a /etc/hostapd/hostapd.conf
 
 echo "DAEMON_CONF='/etc/hostapd/hostapd.conf'" | sudo tee -a /etc/default/hostapd
@@ -60,9 +60,9 @@ echo 'Enable IPv4 routing'
 echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
 
 # to allow this new network to access internet
-sudo iptables -t nat -A POSTROUTING -o enxb827eb8879e7 -j MASQUERADE
-sudo iptables -A FORWARD -i enxb827eb8879e7 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i wlan0 -o enxb827eb8879e7 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 sudo netfilter-persistent save
 
@@ -74,6 +74,3 @@ sudo apt install python3-pip -y
 pip install --upgrade python-iptables
 
 # next: https://github.com/ldx/python-iptables#:~:text=Introduction-,About%20python%2Diptables,rules%20in%20the%20Linux%20kernel.
-
-
-
